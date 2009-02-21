4cfc2806a89bcbd6d0d47e1ad47b0b5ebde11563
Bryan Ash <bryan.a.ash@gmail.com>
2009-02-20 23:51:08 -0500

    Removed unnecessary Wx from feature environment

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,9 @@
 require 'rubygems'
-require 'wx'
 require 'spec'
 
-include Wx
-
-$LOAD_PATH.unshift File.dirname(__FILE__) + "/../../lib"
-
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/../../lib'
 require 'nobbie/wx'
+
 require File.dirname(__FILE__) + '/../../test/suite/example_app'
 
 Nobbie::Wx::ApplicationLauncher.new(ExampleApp.new(false)).run
